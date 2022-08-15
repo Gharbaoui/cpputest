3b0d1aa47d539778192c5985ad272f3c616851ad
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-08-15 10:31:19 +0100

    just add comment in teardown and setup

--- unit_tests/math_tests.cpp
+++ unit_tests/math_tests.cpp
@@ -1,17 +1,28 @@
 #include <CppUTest/TestHarness.h>
 #include "../src/math.hpp"
+#include <iostream>
 
 
 TEST_GROUP(FirstTestGroup)
 {
+	void	setup()
+	{
+		// setup is called prior before each test
+		std::cout << "Setup\n";
+	}
 
+	void	teardown()
+	{
+		// teardown is called after test body
+		std::cout << "Tear down\n";
+	}
 };
 
 
 TEST(FirstTestGroup, First)
 {
 	CHECK_EQUAL(add(2, 3), 5);
-	CHECK_EQUAL(add(2, -2), 0);
+	CHECK_EQUAL(add(2, -2), 1);
 }
 
 TEST(FirstTestGroup, Second)
